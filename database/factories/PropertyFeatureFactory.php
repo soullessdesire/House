47c10ef118bdf6cbee70f40dbeeeff141c345f90
soul32 <soullessdesire34@gmail.com>
2024-12-23 12:45:52 +0300

    Phase 1 for Database Seeding{FactoryCreation}

--- database/factories/PropertyFeatureFactory.php
+++ database/factories/PropertyFeatureFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory <\App\Models\Property>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory <\App\Models\PropertyFeature>
  */
-class PropertyFactory extends Factory
+class PropertyFeatureFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/PropertyImageFactory.php
+++ database/factories/PropertyImageFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory <\App\Models\Property>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory <\App\Models\PropertyImage>
  */
-class PropertyFactory extends Factory
+class PropertyImageFactory extends Factory
 {
     /**
      * Define the model's default state.
